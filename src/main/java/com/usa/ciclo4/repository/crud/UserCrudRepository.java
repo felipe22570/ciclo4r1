63b25339a4576b4bdd7cd67caffc5c26efe7bc68
felipe22570 <piedra22570@gmail.com>
2021-12-09 15:46:07 -0500

    primer commit

---
+++ src/main/java/com/usa/ciclo4/repository/crud/UserCrudRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.usa.ciclo4.repository.crud;
+
+import com.usa.ciclo4.model.User;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author felip
+ */
+public interface UserCrudRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+    //Optional<User> findByName(String name);
+}
